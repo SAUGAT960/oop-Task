06a8fddbf8b4aa9e80ceb73c95db82c45dde8228
SAUGAT960 <robonile4@gmail.com>
2025-06-01 12:15:15 +0545

    Add files via upload

---
+++ CalculateValueOfArrayElement.java
@@ -0,0 +1,11 @@
+package One Dimensional Array;
+
+public class CalculateValueOfArrayElement {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        int sum=0;
+        int arrLength = arr.length;
+        for (int val:arr);
+    }
+    
+}
